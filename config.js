441a8186e2f9909ee8244b92e781676edebf5128
VISHAL KUMAR GUPTA <65849036+vkg2000@users.noreply.github.com>
2022-04-18 02:28:34 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const msalConfig = {
     auth: {
       clientId: 'Zdu7Q~A6BsqcLHIJ5zD7vRbYgLj5eTo_j5.~.',
-      redirectUri: 'http://localhost:8080'
+      redirectUri: 'https://photonicslabiitbbs.netlify.app/'
     }
   };
   
